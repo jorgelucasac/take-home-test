9c71febdd00f3792db2fa13275c593b2d2031555
Jorge Lucas <jorgelucasl91@gmail.com>
2025-12-13 20:50:15 -0300

    chore(docker): remove Dockerfile and update configs
    Removed all content from Dockerfile, disabling Docker build. Commented out HTTPS redirection and authorization in Program.cs. Updated SQL Server connection string password in appsettings.Development.json and added a new DefaultConnection in appsettings.json for the 'sqlserver' instance.

--- backend/src/Fundo.WebApi/Program.cs
+++ backend/src/Fundo.WebApi/Program.cs
@@ -18,9 +18,9 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
-app.UseAuthorization();
+//app.UseAuthorization();
 
 app.MapControllers();
 
