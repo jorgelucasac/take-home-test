5237c9318338aa69c9153c5fcd64c3fc01f4bd25
Jorge Lucas <jorgelucasl91@gmail.com>
2025-12-13 18:18:54 -0300

    feat(api): add ErrorResponse class for standardized errors
    Introduced the ErrorResponse class in the Fundo.WebApi.Transport.Response namespace to standardize error responses. The class includes a message, a list of key-value detail pairs, and a static method to create instances from Error objects. Also added the necessary using directive for Error integration.

---
+++ backend/src/Fundo.WebApi/Transport/Response/ErrorResponse.cs
@@ -0,0 +1,21 @@
+﻿using Fundo.Application.Handlers.Results;
+
+namespace Fundo.WebApi.Transport.Response
+{
+    public class ErrorResponse
+    {
+        public string Message { get; set; }
+        public List<KeyValuePair<string, string>> Details { get; set; } = new();
+
+        private ErrorResponse(string message, List<KeyValuePair<string, string>>? details = null)
+        {
+            Message = message;
+            Details = details ?? [];
+        }
+
+        public static ErrorResponse From(Error error)
+        {
+            return new ErrorResponse(error.Message, error.Details);
+        }
+    }
+}
