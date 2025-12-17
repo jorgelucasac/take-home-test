d337ee08ab7c2ce8783450a6a84ec7406e2907a7
Jorge Lucas <jorgelucasl91@gmail.com>
2025-12-17 19:18:43 -0300

    test(loans): add unit test for LoansService getAll method
    Added loans.service.spec.ts to test LoansService in Angular.
    The test uses TestBed and HttpTestingController to mock HTTP
    requests and verifies that getAll() retrieves loan data from
    the API as expected.

---
+++ frontend/src/tests/http/services/loans.service.spec.ts
@@ -0,0 +1,48 @@
+import { TestBed } from '@angular/core/testing';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+import { environment } from '../../../environments/environment';
+import { LoansService } from '../../../app/services/loans.service';
+import { LoanDto } from '../../../app/dtos/loan.dto';
+import { provideHttpClient } from '@angular/common/http';
+
+describe('LoansService', () => {
+  let service: LoansService;
+  let httpMock: HttpTestingController;
+  const apiUrl = `${environment.loanApiBaseUrl}/loans`;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+        providers: [
+          provideHttpClient(),
+          provideHttpClientTesting()
+        ],
+    });
+
+    service = TestBed.inject(LoansService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('should retrieve all loans from the API', () => {
+    const mockLoans: LoanDto[] = [
+      {
+        id: '1',
+        amount: 5000,
+        currentBalance: 2000,
+        applicantName: 'Alice',
+        status: 'ACTIVE',
+      },
+    ];
+
+    service.getAll().subscribe((loans) => {
+      expect(loans).toEqual(mockLoans);
+    });
+
+    const request = httpMock.expectOne(apiUrl);
+    expect(request.request.method).toBe('GET');
+    request.flush(mockLoans);
+  });
+});
