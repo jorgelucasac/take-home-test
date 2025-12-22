e51cb0b3690c02d641920bbccd6e32aa2362f087
Jorge Lucas <jorgelucasl91@gmail.com>
2025-12-22 19:46:10 -0300

    feat(middleware): add CorrellationMiddleware with app name support
    Replaced CorrellationIdMiddleware with CorrellationMiddleware, which now handles both correlation IDs and application names in request headers. Logging scope is managed using ILogger's BeginScope instead of Serilog's LogContext. Updated middleware extension and adjusted local development URLs in launchSettings.json.

---
+++ backend/src/Fundo.WebApi/Middlewares/CorrellationMiddleware.cs
@@ -0,0 +1,50 @@
+﻿namespace Fundo.WebApi.Middlewares;
+
+public class CorrellationMiddleware
+{
+    private const string CorrelationIdHeaderName = "X-Correlation-Id";
+    private const string CorrelationIdLogProperty = "CorrelationId";
+    private const string ApplicationHeaderName = "X-Application-Name";
+    private const string ApplicationLogProperty = "AppOriginName";
+    private readonly ILogger<CorrellationMiddleware> _logger;
+    private readonly RequestDelegate _next;
+
+    public CorrellationMiddleware(RequestDelegate next, ILogger<CorrellationMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        var correlationId = GetOrCreateCorrelationId(context);
+        var applicationName = GetApplicationName(context);
+
+        using var _ = _logger.BeginScope(new Dictionary<string, string>
+               {
+                   { CorrelationIdLogProperty, correlationId },
+                   { ApplicationLogProperty, applicationName }
+               });
+
+        await _next(context);
+    }
+
+    private static string GetApplicationName(HttpContext context)
+    {
+        if (context.Request.Headers.TryGetValue(ApplicationHeaderName, out var applicationName))
+        {
+            return applicationName.ToString();
+        }
+        return string.Empty;
+    }
+
+    private static string GetOrCreateCorrelationId(HttpContext context)
+    {
+        if (!context.Request.Headers.TryGetValue(CorrelationIdHeaderName, out var correlationId))
+        {
+            correlationId = Guid.NewGuid().ToString();
+        }
+        context.Response.Headers.TryAdd(CorrelationIdHeaderName, correlationId);
+        return correlationId.ToString();
+    }
+}
