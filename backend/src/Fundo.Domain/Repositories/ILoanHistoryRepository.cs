0c4a11807a98cd1d2cbba61a5def00206985d682
Jorge Lucas <jorgelucasl91@gmail.com>
2026-01-13 18:44:03 -0300

    feat: add ILoanHistoryRepository interface

---
+++ backend/src/Fundo.Domain/Repositories/ILoanHistoryRepository.cs
@@ -0,0 +1,8 @@
+﻿using Fundo.Domain.Entities;
+
+namespace Fundo.Domain.Repositories;
+
+public interface ILoanHistoryRepository
+{
+    Task AddAsync(LoanHistory loanHistory, CancellationToken cancellationToken = default);
+}
