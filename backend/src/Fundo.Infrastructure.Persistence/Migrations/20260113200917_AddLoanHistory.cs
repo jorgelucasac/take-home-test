9ba7b23cdeb8189d8548d3abfd542b68b67d6585
Jorge Lucas <jorgelucasl91@gmail.com>
2026-01-13 18:46:27 -0300

    feat: add migration for LoanHistory table

---
+++ backend/src/Fundo.Infrastructure.Persistence/Migrations/20260113200917_AddLoanHistory.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fundo.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLoanHistory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LoanHistories",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    LoanId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CurrentBalance = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LoanHistories", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_LoanHistories_Loans_LoanId",
+                        column: x => x.LoanId,
+                        principalTable: "Loans",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LoanHistories_LoanId",
+                table: "LoanHistories",
+                column: "LoanId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LoanHistories");
+        }
+    }
+}
