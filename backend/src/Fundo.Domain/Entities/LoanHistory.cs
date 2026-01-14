b00c9afdecd0737b8df24096a0917401e8fa3f20
Jorge Lucas <jorgelucasl91@gmail.com>
2026-01-14 14:51:18 -0300

    feat: add PaymentAmount to LoanHistory with validation

--- backend/src/Fundo.Domain/Entities/LoanHistory.cs
+++ backend/src/Fundo.Domain/Entities/LoanHistory.cs
@@ -1,16 +1,24 @@
 ﻿using Fundo.Domain.Enums;
+using Fundo.Domain.Exceptions;
 
 namespace Fundo.Domain.Entities;
 
-public class LoanHistory : BaseEntity
+public class LoanHistory : BaseEntity<int>
 {
-    public LoanHistory(Guid loanId, decimal currentBalance, LoanStatus status)
+    public LoanHistory(Guid loanId, decimal currentBalance, decimal paymentAmount, LoanStatus status)
     {
+        if (paymentAmount <= 0)
+        {
+            throw new DomainArgumentException("paymentAmount must be greater than zero.", nameof(paymentAmount));
+        }
+
         LoanId = loanId;
         CurrentBalance = currentBalance;
         Status = status;
+        PaymentAmount = paymentAmount;
     }
 
+    public decimal PaymentAmount { get; set; }
     public Guid LoanId { get; set; }
     public decimal CurrentBalance { get; private set; }
     public LoanStatus Status { get; set; }
