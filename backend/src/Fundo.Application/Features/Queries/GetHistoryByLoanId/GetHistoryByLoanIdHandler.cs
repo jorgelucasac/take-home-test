d11fc93d345fd134c700f6a7dc43d890d33d40eb
Jorge Lucas <jorgelucasl91@gmail.com>
2026-01-14 18:38:27 -0300

    feat: add paginated loan history query by loan ID

---
+++ backend/src/Fundo.Application/Features/Queries/GetHistoryByLoanId/GetHistoryByLoanIdHandler.cs
@@ -0,0 +1,59 @@
+﻿using Fundo.Application.Features.Shared;
+using Fundo.Application.Pagination;
+using Fundo.Application.Repositories;
+using Fundo.Application.Results;
+using MediatR;
+
+namespace Fundo.Application.Features.Queries.GetHistoryByLoanId;
+
+public sealed class GetHistoryByLoanIdHandler : IRequestHandler<GetHistoryByLoanIdQuery, Result<PaginatedResponse<LoanHistoryResponse>>>
+{
+    private readonly ILoanHistoryRepository _loanHistoryRepository;
+
+    public GetHistoryByLoanIdHandler(ILoanHistoryRepository loanHistoryRepository)
+    {
+        _loanHistoryRepository = loanHistoryRepository;
+    }
+
+    public async Task<Result<PaginatedResponse<LoanHistoryResponse>>> Handle(GetHistoryByLoanIdQuery request, CancellationToken cancellationToken)
+    {
+        var paginatedHistories = await _loanHistoryRepository.GetByLoanIdPagedAsync(
+            request.LoanId,
+            request.Pagination,
+            cancellationToken
+        );
+
+        if (paginatedHistories.IsEmpty)
+        {
+            return BuildEmptyHistoryResponse(request);
+        }
+
+        PaginatedResponse<LoanHistoryResponse> response = BuildPaginatedHistoryResponse(request, paginatedHistories);
+
+        return Result.Success(response);
+    }
+
+    private static PaginatedResponse<LoanHistoryResponse> BuildPaginatedHistoryResponse(GetHistoryByLoanIdQuery request, PaginatedResponse<Domain.Entities.LoanHistory> paginatedHistories)
+    {
+        var historiesResponse = paginatedHistories.Items
+            .Select(LoanHistoryResponse.FromEntity)
+            .ToList();
+
+        var response = PaginatedResponse<LoanHistoryResponse>.Create(
+            historiesResponse,
+            request.Pagination.PageNumber,
+            request.Pagination.PageSize,
+            paginatedHistories.TotalItems);
+        return response;
+    }
+
+    private static Result<PaginatedResponse<LoanHistoryResponse>> BuildEmptyHistoryResponse(GetHistoryByLoanIdQuery request)
+    {
+        return Result.Success(
+            PaginatedResponse<LoanHistoryResponse>.Empty(
+                request.Pagination.PageNumber,
+                request.Pagination.PageSize
+            )
+        );
+    }
+}
---
+++ backend/src/Fundo.Application/Features/Queries/GetHistoryByLoanId/GetHistoryByLoanIdQuery.cs
@@ -0,0 +1,18 @@
+﻿using Fundo.Application.Features.Shared;
+using Fundo.Application.Pagination;
+using Fundo.Application.Results;
+using MediatR;
+
+namespace Fundo.Application.Features.Queries.GetHistoryByLoanId;
+
+public class GetHistoryByLoanIdQuery : IRequest<Result<PaginatedResponse<LoanHistoryResponse>>>
+{
+    public int LoanId { get; }
+    public PaginationQuery Pagination { get; }
+
+    public GetHistoryByLoanIdQuery(int loanId, int pageNumber = 1, int pageSize = 20)
+    {
+        LoanId = loanId;
+        Pagination = new PaginationQuery(pageNumber, pageSize);
+    }
+}
