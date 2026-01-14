8fc5942d44808b6645eae2558628628507945f5c
Jorge Lucas <jorgelucasl91@gmail.com>
2026-01-14 18:34:48 -0300

    feat: add ILoanHistoryRepository interface with pagination

---
+++ backend/src/Fundo.Application/Repositories/ILoanHistoryRepository.cs
@@ -0,0 +1,11 @@
+﻿using Fundo.Application.Pagination;
+using Fundo.Domain.Entities;
+
+namespace Fundo.Application.Repositories;
+
+public interface ILoanHistoryRepository
+{
+    Task AddAsync(LoanHistory loanHistory, CancellationToken cancellationToken = default);
+
+    Task<PaginatedResponse<LoanHistory>> GetByLoanIdPagedAsync(int loanId, PaginationQuery pagination, CancellationToken cancellationToken = default);
+}
