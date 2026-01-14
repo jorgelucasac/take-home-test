28dd6e2cb32f311fb4b968c66276a4c5e63f0fbc
Jorge Lucas <jorgelucasl91@gmail.com>
2026-01-14 15:04:26 -0300

    test: add unit tests for LoanHistory entity

---
+++ backend/src/Fundo.Services.Tests/Unit/Entities/LoanHistoryTests.cs
@@ -0,0 +1,49 @@
+﻿using FluentAssertions;
+using Fundo.Domain.Entities;
+using Fundo.Domain.Enums;
+using Fundo.Domain.Exceptions;
+using System;
+using Xunit;
+
+namespace Fundo.Services.Tests.Unit.Entities
+{
+    public class LoanHistoryTests
+    {
+        [Fact]
+        public void CreateLoanHistory_WithValidParameters_ShouldCreateInstance()
+        {
+            // Arrange
+            var loanId = Guid.NewGuid();
+            decimal currentBalance = 5000m;
+            decimal paymentAmount = 1000m;
+            LoanStatus status = LoanStatus.Active;
+            // Act
+            var loanHistory = new LoanHistory(loanId, currentBalance, paymentAmount, status);
+            // Assert
+            loanHistory.LoanId.Should().Be(loanId);
+            loanHistory.CurrentBalance.Should().Be(currentBalance);
+            loanHistory.PaymentAmount.Should().Be(paymentAmount);
+            loanHistory.Status.Should().Be(status);
+            loanHistory.Id.Should().Be(0);
+            loanHistory.CreatedAt.Should().BeCloseTo(DateTime.UtcNow, TimeSpan.FromSeconds(1));
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-1)]
+        [InlineData(-1000)]
+        public void CreateLoanHistory_WithInvalidAmount_ShouldThrowException(int amount)
+        {
+            // Arrange & Act
+            Action act = () => new LoanHistory(
+                Guid.NewGuid(),
+                1000m,
+                amount,
+               LoanStatus.Active);
+
+            // Assert
+            act.Should().Throw<DomainArgumentException>()
+                .WithMessage("paymentAmount must be greater than zero.*");
+        }
+    }
+}
